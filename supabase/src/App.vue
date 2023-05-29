e14ab31a8969b0bc06de579fc169831dce5e9021
pinrongc <pinrongc156@gmail.com>
2023-05-28 22:43:51 -0400

    .

--- supabase/src/App.vue
+++ supabase/src/App.vue
@@ -1,27 +1,24 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref, onMounted } from 'vue'
 import { supabase } from './supabase'
 
 const id = ref([])
 async function getId() {
   const { data } = await supabase.from('id').select()
   id.value = data
+
+  onMounted(() => {
+    getCountries()
+  })
 }
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <ul>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <li v-for="id in ids"></li>
+  </ul>
 </template>
 
-<style scoped></style>
+
--- supabase/src/supabase.js
+++ supabase/src/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://tycyeazacauuvgyyusix.supabase.co',
-  'sbp_2cd8730cc70abecf8efa6c76f29457b658276531'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5Y3llYXphY2F1dXZneXl1c2l4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM3NDA4NzEsImV4cCI6MTk5OTMxNjg3MX0.Je-5zYPvpgfiXWjKsGw88y7UGb9Bbuoy8aeaD6ZxOlQ'
 )
