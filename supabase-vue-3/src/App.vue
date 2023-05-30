0dde95e60b68d1e4e9809fc705f0dfc3692a87d5
joannec34 <joannechen9058@gmail.com>
2023-05-30 14:00:44 -0400

    yas

--- supabase-vue-3/src/App.vue
+++ supabase-vue-3/src/App.vue
@@ -1,34 +1,31 @@
 <script setup>
+import { onMounted, ref, computed } from "vue";
+import Account from "./components/Account.vue";
+import Auth from "./components/auth.vue";
+import { supabase } from "./supabase";
+import { useUserStore } from "./stores/userStore";
 
-import { onMounted, ref, computed } from 'vue'
-import Account from './components/Account.vue'
-import Auth from './components/auth.vue'
-import { supabase } from './supabase'
-import { useUserStore } from './stores/userStore'
-
-const session = ref()
-const store = useUserStore()
+const session = ref();
+const store = useUserStore();
 
 onMounted(() => {
   supabase.auth.getSession().then(({ data }) => {
-    session.value = data.session
-  })
+    session.value = data.session;
+  });
 
   supabase.auth.onAuthStateChange((_, _session) => {
-    session.value = _session
-  })
-})
+    session.value = _session;
+  });
+});
 
-console.log(store.data)
-console.log(session)
+console.log(store.data);
+console.log(session);
 </script>
 
 <template>
   <div class="container" style="padding: 50px 0 100px 0">
     <Account v-if="session" :session="session" />
     <Auth v-else />
-    <div id="emptyContainer">
-
-    </div>
+    <div id="emptyContainer"></div>
   </div>
 </template>
--- supabase-vue-3/src/views/Dashboard.vue
+++ supabase-vue-3/src/views/Dashboard.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-      <h1>Welcome to our Dashboard Page</h1>
-      <button @click.prevent="signOut">Sign out</button>
-      <p>Welcome: {{ userEmail }}</p>
-    </div>
-  </template>
+  <div>
+    <h1>Welcome to our Dashboard Page</h1>
+    <img :scr="logo" />
+    <button @click.prevent="signOut">Sign out</button>
+    <p>Welcome: {{ userEmail }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      logo: supabeats.jpg,
+    };
+  },
+};
+</script>
