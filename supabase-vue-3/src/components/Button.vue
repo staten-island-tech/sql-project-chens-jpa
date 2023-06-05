691d53fc0c3be3661fd93252ca9ef5b176e883e6
joannec34 <joannechen9058@gmail.com>
2023-06-04 22:44:25 -0400

    gangangang
    help me

---
+++ supabase-vue-3/src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <button>favorite</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+};
+</script>
+
+<style scoped>
+button {
+  padding: 10px;
+  border: none;
+  transition: 0.2s;
+  background: none;
+}
+
+button:hover {
+  transform: scale(1.1);
+}
+</style>
---
+++ supabase-vue-3/src/stores/musicStore.js
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import { reactive } from "vue";
+import { supabase } from "../supabase.js";
+
+const { data, error } = await supabase.from("store").select();
+
+export const useMusicStore = defineStore("music", {
+  state: () => {
+    return {
+      data: data,
+      error: error,
+    };
+  },
+});
+
+export const musicStore = reactive({
+  albums: [],
+});
+
+data.forEach((e) => {
+  musicStore.albums.push(e);
+});
+
+console.log(musicStore);
